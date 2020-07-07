b245da5177b91fe76fe2e2fab6ad9908e3e3041c
mbrodt <madsbrodt3@gmail.com>
2020-07-07 23:32:37 +0200

    add generic linkbutton component

---
+++ components/LinkButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button
+    :type="type"
+    @click="onClick"
+    class="btn w-full"
+    :class="{ 'bg-gray-800': disabled }"
+    :disabled="disabled"
+  >
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  props: ['disabled', 'type'],
+  methods: {
+    onClick() {
+      this.$emit('click')
+    },
+  },
+}
+</script>
