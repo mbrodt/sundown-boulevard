873121615c1e15f595de6abf7b97c4108d757d21
mbrodt <madsbrodt3@gmail.com>
2020-07-07 23:30:52 +0200

    add tailwind and basic styles

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/*
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
+module.exports = {
+  theme: {
+    fontFamily: {
+      sans: ['Helvetica Neue'],
+    },
+    extend: {
+      colors: {
+        primary: '#BA2329',
+        secondary: '#007DDB',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+  purge: {
+    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js',
+    ],
+  },
+}
