1f686802092ae72c0021dc3d0b673ac2891a4f9e
mbrodt <madsbrodt3@gmail.com>
2020-07-07 23:33:30 +0200

    create homegrid layout

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home-grid">
+    <div class="col-start-1 col-span-8 shadow-2xl">
+      <DishCarousel />
+    </div>
+    <div class="col-start-9 col-span-4 bg-white shadow-2xl rounded-lg">
+      <StartOrder />
+    </div>
+    <div class="col-start-1 col-span-6 bg-white shadow-2xl rounded-lg">
+      <FindOrder />
+    </div>
+    <div class="col-start-7 col-span-6 bg-white shadow-2xl rounded-lg">
+      <ContentBox />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.home-grid {
+  display: grid;
+  grid-gap: 32px;
+  grid-template-columns: repeat(12, 1fr);
+  grid-template-rows: repeat(2, 400px);
+}
+</style>
