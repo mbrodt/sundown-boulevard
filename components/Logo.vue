54bc6ba5d8e5fd74b12f139dc25de9a55e0ba0eb
mbrodt <madsbrodt3@gmail.com>
2020-07-07 23:32:47 +0200

    add navigation

---
+++ components/Logo.vue
@@ -0,0 +1,6 @@
+<template>
+  <nuxt-link to="/" class="flex items-center">
+    <img class="w-16 h-16 mb-4 mr-2" src="~assets/images/logo.svg" alt="" />
+    <span class="font-bold text-2xl">Sundown Boulevard</span>
+  </nuxt-link>
+</template>
---
+++ components/Navigation.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex justify-between">
+    <Logo />
+    <ul class="flex items-center space-x-4 text-lg font-bold uppercase">
+      <li
+        class="border-b-2 border-transparent hover:border-primary cursor-pointer"
+      >
+        Restaurants
+      </li>
+      <li
+        class="border-b-2 border-transparent hover:border-primary cursor-pointer"
+      >
+        Products
+      </li>
+      <li
+        class="border-b-2 border-transparent hover:border-primary cursor-pointer"
+      >
+        Newsletter
+      </li>
+      <li
+        class="border-b-2 border-transparent hover:border-primary cursor-pointer"
+      >
+        Contact
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
