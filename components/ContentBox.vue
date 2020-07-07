e6201ede50b0661f42a2d4fc268e88e12c236eed
mbrodt <madsbrodt3@gmail.com>
2020-07-07 23:33:06 +0200

    add basic layout and components from home screen

---
+++ components/ContentBox.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="p-24 flex flex-col justify-center h-full">
+    <h2 class="mb-4">Welcome to Sundown Boulevard</h2>
+    <p>
+      We have a lot of great bacon. Bacon ipsum dolor amet capicola salami rump,
+      picanha brisket kielbasa burgdoggen. Pancetta alcatra doner flank. Flank
+      meatloaf pork loin beef pancetta t-bone pork belly venison fatback biltong
+      tongue shankle.
+    </p>
+  </div>
+</template>
---
+++ components/StartOrder.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="px-12 py-8 flex flex-col h-full justify-around text-center">
+    <h2>Start your order here</h2>
+    <p>
+      Sundown Boulevard is ready to welcome you with our amazing dishes and
+      fabulous drinks. Create your order in just a few short steps!
+    </p>
+    <LinkButton @click="onClick" target="/pick-dish">Let's go</LinkButton>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    onClick() {
+      this.$router.push({
+        path: '/pick-dish',
+      })
+    },
+  },
+}
+</script>
+
+<style scoped></style>
