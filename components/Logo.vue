175598de66c018dddbb46b2b1a3cedca0371e12e
mbrodt <madsbrodt3@gmail.com>
2020-07-09 00:05:18 +0200

    add transitions on hover to logo and nav

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,6 +1,13 @@
 <template>
-  <nuxt-link to="/" class="flex items-center">
-    <img class="w-16 h-16 mb-4 mr-2" src="~assets/images/logo.svg" alt="" />
+  <nuxt-link
+    to="/"
+    class="flex items-center transform transition duration-150 ease hover:scale-110"
+  >
+    <img
+      class="w-16 h-16 mb-4 mr-2"
+      src="~assets/images/logo.svg"
+      alt="Company Logo"
+    />
     <span class="font-bold text-2xl">Sundown Boulevard</span>
   </nuxt-link>
 </template>
--- components/Navigation.vue
+++ components/Navigation.vue
@@ -3,22 +3,22 @@
     <Logo />
     <ul class="flex items-center space-x-4 text-lg font-bold uppercase">
       <li
-        class="border-b-2 border-transparent hover:border-primary cursor-pointer"
+        class="border-b-2 border-transparent hover:border-primary cursor-pointer transform transition duration-150 ease hover:scale-110"
       >
         Restaurants
       </li>
       <li
-        class="border-b-2 border-transparent hover:border-primary cursor-pointer"
+        class="border-b-2 border-transparent hover:border-primary cursor-pointer transform transition duration-150 ease hover:scale-110"
       >
         Products
       </li>
       <li
-        class="border-b-2 border-transparent hover:border-primary cursor-pointer"
+        class="border-b-2 border-transparent hover:border-primary cursor-pointer transform transition duration-150 ease hover:scale-110"
       >
         Newsletter
       </li>
       <li
-        class="border-b-2 border-transparent hover:border-primary cursor-pointer"
+        class="border-b-2 border-transparent hover:border-primary cursor-pointer transform transition duration-150 ease hover:scale-110"
       >
         Contact
       </li>
